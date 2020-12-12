d9763a2ec862b396468510a5e5abff2bcb7135c5
lubianat <tiago.lubiana.alves@usp.br>
2020-12-12 11:52:21 -0300

    code: Receive arguments via argparse

--- term2subject.py
+++ term2subject.py
@@ -1,19 +1,36 @@
 import pandas as pd
 from pandas import json_normalize 
+from gooey import Gooey
 import requests
 from wikidata2df import wikidata2df
+import argparse
 
 
+# Simple GUI based on Gooey
+#
+#
+# https://codeburst.io/how-to-use-the-easiest-gui-of-your-life-in-python-d3762270a2a0
 
+#@Gooey
 def main():
-    term = input("Enter a name to look for titles: " )
-    term_id = input("Enter Wikidata QID for: " )
+    parser = argparse.ArgumentParser(description='Do stuff with strings.')
+    parser.add_argument('-a','--term', required=False, nargs='+') 
+    parser.add_argument('-m','--term_id', required=False, nargs='+')
 
-    def run_all(term, term_id):
+    args = vars(parser.parse_args())
+
+
+    def run_all_to_file(term, term_id):
         url = prepare_url_for_search(term)
         ids =  pull_related_ids(url)
         articles_dataframe = filter_for_instances_of_article(ids)
         print_qs_to_file(articles_dataframe, term, term_id)
+
+    def run_all_to_prompt(term, term_id):
+        url = prepare_url_for_search(term)
+        ids =  pull_related_ids(url)
+        articles_dataframe = filter_for_instances_of_article(ids)
+        print_qs_to_prompt(articles_dataframe, term, term_id)
 
     def prepare_url_for_search(term):
         term_for_url = term.replace(" ","%20")
@@ -60,8 +77,20 @@ def main():
                 r = "|S887|"
                 ro = "Q69652283"
                 f.write(s + p + o + r + ro + "\n")
-    run_all(term, term_id)
 
+    def print_qs_to_prompt(articles_dataframe, term, term_id):
+        with open(term + ".qs", "w+") as f:
+            for i, row in articles_dataframe.iterrows():
+                s = row["item"]
+                p = "|P921|"
+                o = term_id
+                r = "|S887|"
+                ro = "Q69652283"
+                print(s + p + o + r + ro + "\n")
+
+
+    if args["term"] and args["term_id"]:
+        run_all_to_prompt(args["term"][0], args["term_id"][0])
 
 
 if __name__ == "__main__":
