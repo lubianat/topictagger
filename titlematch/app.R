9d257e272e75347bbe5531d73480147a4066bf66
jvfe <jvfecav@gmail.com>
2021-02-07 10:03:54 -0300

    style: Run styler on source code
    A consistent style guide facilitates collaboration.

--- titlematch/app.R
+++ titlematch/app.R
@@ -1,120 +1,129 @@
 library(shiny)
-source("utils.R")
 library(rclipboard)
+source("utils.R")
 
 ui <- fluidPage(
 
-    rclipboardSetup(),
-    windowTitle = "TopicTagger",
-    title = tags$head(tags$link(rel="icon", 
-                                href="https://raw.githubusercontent.com/lubianat/titlematch/master/assets/favicon-32x32.png", 
-                                type="image/x-icon"),
+  rclipboardSetup(),
+  windowTitle = "TopicTagger",
+  title = tags$head(tags$link(
+    rel = "icon",
+    href = "https://raw.githubusercontent.com/lubianat/titlematch/master/assets/favicon-32x32.png",
+    type = "image/x-icon"
+  ), ),
+
+  sidebarLayout(
+    sidebarPanel(
+      textInput(
+        inputId = "term",
+        label = "Term",
+        value = "",
+        width = NULL,
+        placeholder = NULL
+      ),
+      dataTableOutput("candidate_qids"),
+      uiOutput("search"),
+      textInput(
+        inputId = "term_qid",
+        label = "Term QID",
+        value = "",
+        width = NULL,
+        placeholder = NULL
+      ),
+      numericInput(
+        inputId = "n_articles",
+        label = "Number of articles",
+        value = 200,
+        min = 1,
+        max = 200,
+        step = 1
+      )
     ),
+    mainPanel(
 
-    sidebarLayout(
-        sidebarPanel(
-            textInput(inputId="term",
-                      label="Term",
-                      value = "",
-                      width = NULL,
-                      placeholder = NULL),
-            dataTableOutput('candidate_qids'),
-            uiOutput("search"),
-            textInput(inputId="term_qid",
-                      label="Term QID",
-                      value = "",
-                      width = NULL,
-                      placeholder = NULL),
-            numericInput(inputId="n_articles", 
-                         label="Number of articles", 
-                         value=200, 
-                         min = 1, 
-                         max = 200, 
-                         step = 1)
-            
-            
-        ),
-        mainPanel(
-            
-            # UI ouputs for the copy-to-clipboard buttons
-            uiOutput("clip"),
-            tags$a(href="https://quickstatements.toolforge.org/#/batch", "Go to Quickstatements!"),
-            textOutput("summary"),
-            verbatimTextOutput("qs") 
-            ),
-
-
-    )
+      # UI ouputs for the copy-to-clipboard buttons
+      uiOutput("clip"),
+      tags$a(href = "https://quickstatements.toolforge.org/#/batch", "Go to Quickstatements!"),
+      textOutput("summary"),
+      verbatimTextOutput("qs")
+    ),
+  )
 )
 
 server <- function(input, output) {
+  output$search <- renderUI({
+    term <- input$term
+    url <- a("Search on Wikidata",
+      href = paste0("https://www.wikidata.org/w/index.php?search=", term)
+    )
+    print("Here")
+    tagList("URL link:", url)
+  })
+  output$summary <- renderText({
+    term <- input$term
+    n <- input$n_articles
+    paste("Obtaining up to", n, "articles about", term)
+  })
 
-    
-    output$search <-renderUI({
-        term = input$term
-        url = a("Search on Wikidata", 
-                href=paste0("https://www.wikidata.org/w/index.php?search=", term))
-        print("Here")
-        tagList("URL link:", url)
-        
-    })
-    output$summary <- renderText({
-        term = input$term
-        n = input$n_articles
-        paste("Obtaining up to", n, "articles about", term)
-    })
-    
-    
-    output$candidate_qids <- renderDataTable({
-        term = input$term
-        term_qid = input$term_qid
-        n_articles = input$n_articles
-        url = prepare_url_for_search(term, n_results = n_articles)
-        ids = pull_related_ids(url)
-        articles = filter_for_instances_of_article(ids)
-        descriptions = get_top_descriptions(ids=ids,
-                                            article_ids= articles)
-        descriptions$itemDescription = NULL
-        return(descriptions)
-    }, escape = FALSE, options = list(dom = 't'))
-    
-    
-    output$qs <- renderText({
-        term = input$term
-        term_qid = input$term_qid
-        n_articles = input$n_articles
-        url = prepare_url_for_search(term, n_results = n_articles)
-        ids = pull_related_ids(url)
-        articles = filter_for_instances_of_article(ids)
-        result = prepare_qs_to_render(article_qids=articles,
-                           term=term,
-                           term_id=term_qid)
-        
-        if (term_qid == ""){
-            return("Waiting for a term QID")
-        } else {
-            return(result)
-        }
-    })
-    
-    
-    output$clip <- renderUI({
-        term = input$term
-        term_qid = input$term_qid
-        n_articles = input$n_articles
-        url = prepare_url_for_search(term, n_results = n_articles)
-        ids = pull_related_ids(url)
-        articles = filter_for_instances_of_article(ids)
-        result = prepare_qs_to_render(article_qids=articles,
-                                      term=term,
-                                      term_id=term_qid)
-        
-        result = paste(result, collapse="")
-        rclipButton("clipbtn", "Copy", result, icon("clipboard"))
-    })
-    
-    
+
+  output$candidate_qids <- renderDataTable(
+    {
+      term <- input$term
+      term_qid <- input$term_qid
+      n_articles <- input$n_articles
+      url <- prepare_url_for_search(term, n_results = n_articles)
+      ids <- pull_related_ids(url)
+      articles <- filter_for_instances_of_article(ids)
+      descriptions <- get_top_descriptions(
+        ids = ids,
+        article_ids = articles
+      )
+      descriptions$itemDescription <- NULL
+      return(descriptions)
+    },
+    escape = FALSE,
+    options = list(dom = "t")
+  )
+
+
+  output$qs <- renderText({
+    term <- input$term
+    term_qid <- input$term_qid
+    n_articles <- input$n_articles
+    url <- prepare_url_for_search(term, n_results = n_articles)
+    ids <- pull_related_ids(url)
+    articles <- filter_for_instances_of_article(ids)
+    result <- prepare_qs_to_render(
+      article_qids = articles,
+      term = term,
+      term_id = term_qid
+    )
+
+    if (term_qid == "") {
+      return("Waiting for a term QID")
+    } else {
+      return(result)
+    }
+  })
+
+
+  output$clip <- renderUI({
+    term <- input$term
+    term_qid <- input$term_qid
+    n_articles <- input$n_articles
+    url <- prepare_url_for_search(term, n_results = n_articles)
+    ids <- pull_related_ids(url)
+    articles <- filter_for_instances_of_article(ids)
+    result <- prepare_qs_to_render(
+      article_qids = articles,
+      term = term,
+      term_id = term_qid
+    )
+
+    result <- paste(result, collapse = "")
+    rclipButton("clipbtn", "Copy", result, icon("clipboard"))
+  })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
--- titlematch/utils.R
+++ titlematch/utils.R
@@ -5,15 +5,16 @@ library(WikidataQueryServiceR)
 #' Prepare URL for Wikidata search
 #'
 #' @param term A string to search on Wikidata
-#' @param n_results The number of search results. Defaults to 200. 
-prepare_url_for_search <- function(term, n_results=200)
-{
-  term_for_url = str_replace(term," ","%20")
-  term_for_url = paste0("%22", term_for_url, "%22")
-  
-  url = paste0("https://www.wikidata.org/w/api.php?action=query&list=search&srsearch=",
-               term_for_url,
-               "&srlimit=", n_results, "&srprop=size&formatversion=2&format=json")
+#' @param n_results The number of search results. Defaults to 200.
+prepare_url_for_search <- function(term, n_results = 200) {
+  term_for_url <- str_replace(term, " ", "%20")
+  term_for_url <- paste0("%22", term_for_url, "%22")
+
+  url <- paste0(
+    "https://www.wikidata.org/w/api.php?action=query&list=search&srsearch=",
+    term_for_url,
+    "&srlimit=", n_results, "&srprop=size&formatversion=2&format=json"
+  )
   return(url)
 }
 
@@ -21,12 +22,11 @@ prepare_url_for_search <- function(term, n_results=200)
 #' Pull IDs returned in a Wikidata Search
 #'
 #' @param url A query URL that returns a JSON object
-pull_related_ids <- function(url)
-{
-  resp = GET(url)
-  obj = jsonlite::fromJSON(content(resp, "text"), simplifyVector = FALSE)
-  search = obj[["query"]][["search"]]
-  ids = c()
+pull_related_ids <- function(url) {
+  resp <- GET(url)
+  obj <- jsonlite::fromJSON(content(resp, "text"), simplifyVector = FALSE)
+  search <- obj[["query"]][["search"]]
+  ids <- c()
   for (item in search)
   {
     ids <- c(ids, item$title)
@@ -39,62 +39,59 @@ pull_related_ids <- function(url)
 #' @param string A string
 #' @param len The length of the stub to return
 #' @param ending The end of the stub. Defaults to "..."
-make_stub <- function(string, len, ending="...")
-{
-  if (nchar(string)<50){
+make_stub <- function(string, len, ending = "...") {
+  if (nchar(string) < 50) {
     return(string)
   } else {
-    paste0(substr(string,1,len), ending)
+    paste0(substr(string, 1, len), ending)
   }
 }
 
 #' Get labels and descriptions of top 5
 #'
 #' @param ids A list of Wikidata ids
-get_top_descriptions <- function(ids, article_ids)
-{    
-  
-  counter = 0
-  items = "{"
-  for(i in ids[!(ids %in% article_ids)])
+get_top_descriptions <- function(ids, article_ids) {
+  counter <- 0
+  items <- "{"
+  for (i in ids[!(ids %in% article_ids)])
   {
-    items = paste0(items, " wd:", i)
-    counter = counter +1
-    
-    if (counter == 5){
+    items <- paste0(items, " wd:", i)
+    counter <- counter + 1
+
+    if (counter == 5) {
       break
     }
   }
-  items = paste0(items, " }")
-  
-  
-  descriptions_query = paste0(' SELECT ?item ?itemLabel ?itemDescription ?typeLabel
+  items <- paste0(items, " }")
+
+
+  descriptions_query <- paste0(" SELECT ?item ?itemLabel ?itemDescription ?typeLabel
             WHERE
             {
-            VALUES ?item ', items, '.
+            VALUES ?item ", items, '.
             ?item wdt:P31 ?type .
             SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
             }
             ')
-  
-  descriptions_dataframe = query_wikidata(descriptions_query)
-  
-  links = c()
+
+  descriptions_dataframe <- query_wikidata(descriptions_query)
+
+  links <- c()
   for (u in descriptions_dataframe[["item"]])
   {
-    qid =str_replace(u, "http://www.wikidata.org/entity/", "")
-    link = paste0("<a href=",u,">", qid, "</a>")
-    links = c(links, link)
+    qid <- str_replace(u, "http://www.wikidata.org/entity/", "")
+    link <- paste0("<a href=", u, ">", qid, "</a>")
+    links <- c(links, link)
   }
-  descriptions_dataframe[["item"]] = links
-  
-  descriptions = c()
+  descriptions_dataframe[["item"]] <- links
+
+  descriptions <- c()
   for (u in descriptions_dataframe[["itemDescription"]])
   {
-    description = make_stub(u, len=50)
-    descriptions = c(descriptions, description)
+    description <- make_stub(u, len = 50)
+    descriptions <- c(descriptions, description)
   }
-  descriptions_dataframe[["itemDescription"]] = descriptions
+  descriptions_dataframe[["itemDescription"]] <- descriptions
   return(descriptions_dataframe)
 }
 
@@ -104,32 +101,31 @@ get_top_descriptions <- function(ids, article_ids)
 #' Select which QIDs on a list are instances of article
 #'
 #' @param ids A list of Wikidata ids
-filter_for_instances_of_article <- function(ids)
-{    
-  items = "{"
-  for(i in ids)
+filter_for_instances_of_article <- function(ids) {
+  items <- "{"
+  for (i in ids)
   {
-    items = paste0(items, " wd:", i)
+    items <- paste0(items, " wd:", i)
   }
-  items = paste0(items, " }")
-  
-  
-  articles =paste0(' SELECT ?item ?itemLabel
+  items <- paste0(items, " }")
+
+
+  articles <- paste0(" SELECT ?item ?itemLabel
             WHERE
             {
-            VALUES ?item ', items, '.
+            VALUES ?item ", items, '.
             ?item wdt:P31 wd:Q13442814.
             SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
             }
             ')
-  
-  articles_dataframe = query_wikidata(articles)
-  
-  result = c()
+
+  articles_dataframe <- query_wikidata(articles)
+
+  result <- c()
   for (u in articles_dataframe[["item"]])
   {
-    qid =str_replace(u, "http://www.wikidata.org/entity/", "")
-    result = c(result, qid)
+    qid <- str_replace(u, "http://www.wikidata.org/entity/", "")
+    result <- c(result, qid)
   }
   return(result)
 }
@@ -139,15 +135,14 @@ filter_for_instances_of_article <- function(ids)
 #' @param term A string that was searched on Wikidata
 #' @param term_id The QID relative to the term parameter
 #' @param article_qids A vector of QIDs of articles that have the term as main subject
-print_qs_to_prompt <- function(article_qids, term, term_id)
-{
+print_qs_to_prompt <- function(article_qids, term, term_id) {
   for (article in article_qids)
   {
-    s = article
-    p = "|P921|"
-    o = term_id
-    r = "|S887|"
-    ro = "Q69652283"
+    s <- article
+    p <- "|P921|"
+    o <- term_id
+    r <- "|S887|"
+    ro <- "Q69652283"
     print(paste0(s, p, o, r, ro))
   }
 }
@@ -158,23 +153,17 @@ print_qs_to_prompt <- function(article_qids, term, term_id)
 #' @param term A string that was searched on Wikidata
 #' @param term_id The QID relative to the term parameter
 #' @param article_qids A vector of QIDs of articles that have the term as main subject
-prepare_qs_to_render <- function(article_qids, term, term_id)
-{
-  result=c("\n")
+prepare_qs_to_render <- function(article_qids, term, term_id) {
+  result <- c("\n")
   for (article in article_qids)
   {
-    s = article
-    p = "|P921|"
-    o = term_id
-    r = "|S887|"
-    ro = "Q69652283"
-    statement = paste0(s, p, o, r, ro)
-    result=c(result, paste0(statement,"\n" ))
+    s <- article
+    p <- "|P921|"
+    o <- term_id
+    r <- "|S887|"
+    ro <- "Q69652283"
+    statement <- paste0(s, p, o, r, ro)
+    result <- c(result, paste0(statement, "\n"))
   }
   return(result)
 }
-
-
-
-
-
